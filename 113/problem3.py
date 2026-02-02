601d6033e813e51a9e7bde53b64c691385c9049e
sean0505tsai <80439835+sean0505tsai@users.noreply.github.com>
2026-02-03 03:02:56 +0800

    renamed 113-p4 and p3 in progress

---
+++ 113/problem3.py
@@ -0,0 +1,22 @@
+def check(x, data:dict, label:dict):
+    if len(x)==0: return
+    for key, value in label.items():
+        if x[0] in value and x[0] in label['num']:
+            data['num'] += int(x[0])+1
+        elif x[0] in value: data[key] += 2
+    return check(x[1:], data, label)
+
+def countSymbol():
+    num = [str(i) for i in range(1, 10)]
+    special, alpha = ['~','!','@','#','$','%'], ['a', 'b', 'c', 'd']
+    data = {'num': 0, 'special': 0, 'alpha': 0}
+    label = {'num': num, 'special': special, 'alpha': alpha}
+    check('_5f2xe3r!1^c#', data, label)
+    for key in data.keys(): print(key, end='')      # problem 3-1
+    for value in data.values(): print(value, end='')  # problem 3-2
+
+def countWord(sentence):
+    words = sentence.split()
+    data = {w:words.count(w) for w in set(words)}
+    info = sorted(data.items(), key=lambda d:d[1])
+    for i in info: print(i, end='')                 # problem 3-3
